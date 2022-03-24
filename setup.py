afee3da1ecb8517f1eb03e3b6c4857d3d79879c6
Esben Jannik Bjerrum <esben.bjerrum@astrazeneca.com>
2022-03-24 08:57:03 +0100

    Update setup.py
    Updated license info and version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="levenshteinaugment",
-    version="0.0.1",
+    version="0.1.0",
     description="SLURM distributed Levenshtein augmentation of datasets",
     author="Molecular AI group",
     author_email="esben.bjerrum@astrazeneca.com",
-    license="proprietary",
+    license="Apache 2.0",
     packages=find_packages(exclude=("tests",)),
 )
